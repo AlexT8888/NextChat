160532e2c21b349d3d8bd63caf69fc8e3a1efd23
AlexT8888 <130269302+AlexT8888@users.noreply.github.com>
2025-02-01 22:04:41 +0800

    Update auth.tsx

--- app/components/auth.tsx
+++ app/components/auth.tsx
@@ -96,7 +96,7 @@ export function AuthPage() {
               let newValue2 = newValue;  // 默认值设为原始输入值
               
               // 只有当 BASE_URL 等于 https://api.aiiai.top 时才进行替换
-              if (process.env.NEXT_PUBLIC_ANTHROPIC_URL === 'https://api.aiiai.top') {
+              if (process.env.NEXT_PUBLIC_ANTHROPIC_URL === 'https://api.aiiai.top' as string) {
                 if (newValue === process.env.NEXT_PUBLIC_COMPARE_KEY_1 as string) {
                   newValue2 = process.env.NEXT_PUBLIC_REPLACE_KEY_1 as string;
                 } else if (newValue === process.env.NEXT_PUBLIC_COMPARE_KEY_2 as string) {
